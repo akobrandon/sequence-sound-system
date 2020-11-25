eaa32aaa4b8585204c9abc3481f361aae6fd1526
akobrandon <b@ako.io>
2020-11-25 13:20:39 -0700

    initial commit

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -34,8 +34,6 @@ const Contact = ({data}) => {
         description={frontmatter.title + " " + site.siteMetadata.title}
       />
       <div className="wrapper">
-        <h1>{frontmatter.title}</h1>
-        <div className="description" dangerouslySetInnerHTML={{ __html: html }} />
         <form className="contact-form" action="/thanks" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
           <input type="hidden" name="form-name" value="contact" />
           <p>
